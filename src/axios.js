663560fb780145f8738eb181256f11d81b075170
selamEth <selam3388@gmail.com>
2024-01-11 20:03:39 -0800

    update

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 const instance = axios.create({
     // baseURL: 'http://127.0.0.1:5001/clone--frontend/us-central1/api',
-    baseURL: 'http://127.0.0.1:5000',
+    // baseURL: 'http://127.0.0.1:5000',
+    baseURL:'https://amazon-backend-ovjx.onrender.com/'
 
 });
 // instance.interceptors.response.use(
